c0f752b728d7604007484d2abb1943929432bb61
bangjaeyoung <qkdwo4613@gmail.com>
2022-11-29 18:16:11 +0900

    first commit

---
+++ src/main/java/com/example/coffeeorderingwebapp/CoffeeOrderingWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeorderingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderingWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOrderingWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coffeeorderingwebapp/CoffeeOrderingWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeeorderingwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOrderingWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
