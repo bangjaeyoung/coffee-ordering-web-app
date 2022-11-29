e1969c356bd77332412ad960b20be631a90cc490
bangjaeyoung <qkdwo4613@gmail.com>
2022-11-30 00:04:37 +0900

    Impl Controller

---
+++ src/main/java/com/example/coffeeorderingwebapp/coffee/CoffeeController.java
@@ -0,0 +1,38 @@
+package com.example.coffeeorderingwebapp.coffee;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v1/coffees")
+public class CoffeeController {
+    @PostMapping
+    public ResponseEntity postCoffee(@RequestParam("engName") String engName,
+                                     @RequestParam("korName") String korName,
+                                     @RequestParam("price") int price) {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("engName", engName);
+        map.put("korName", korName);
+        map.put("price", price);
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{coffee-id}")
+    public ResponseEntity getCoffee(@PathVariable("coffee-id") long coffeeId) {
+        System.out.println("# coffee-id: " + coffeeId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity getCoffees() {
+        System.out.println("# get Coffees");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
