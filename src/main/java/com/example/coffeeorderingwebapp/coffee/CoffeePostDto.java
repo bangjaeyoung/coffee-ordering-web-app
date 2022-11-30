bf015dd287cdbca59050165ab6f4407a692b49c9
bangjaeyoung <qkdwo4613@gmail.com>
2022-12-01 00:52:54 +0900

    Impl Dto

---
+++ src/main/java/com/example/coffeeorderingwebapp/coffee/CoffeePostDto.java
@@ -0,0 +1,19 @@
+package com.example.coffeeorderingwebapp.coffee;
+
+public class CoffeePostDto {
+    private String engName;
+    private String korName;
+    private int price;
+
+    public String getEngName() {
+        return engName;
+    }
+
+    public String getKorName() {
+        return korName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
